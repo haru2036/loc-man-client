7dfc2db2c5b13f7e4e0d59ab83e10035e73080bd
haru2036 <haru2036@gmail.com>
2015-12-17 23:54:29 +0900

    fix WebSocketActivity (but it's so dirty)

--- app/src/main/scala/com/haru2036/locman/app/WebSocketActivity.scala
+++ app/src/main/scala/com/haru2036/locman/app/WebSocketActivity.scala
@@ -1,11 +1,19 @@
 package com.haru2036.locman.app
 
-import android.app.Activity
-import android.os.Bundle
-import android.widget._
-import macroid._
+import java.net.URI
+import java.util
 
-class WebSocketActivity extends Activity with Contexts[Activity]{
+import android.app.Activity
+import android.os.{AsyncTask, Handler, Bundle}
+import android.util.Log
+import android.widget._
+import com.neovisionaries.ws.client._
+import macroid._
+import scalaz.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
+
+class WebSocketActivity extends Activity with Contexts[Activity] {
     lazy val arrayAdapter = new ArrayAdapter[String](getBaseContext, R.layout.item_list_websocket_test, R.id.textView)
     lazy val listView : ListView = {
         val view = new ListView(getApplicationContext)
@@ -14,9 +22,84 @@ class WebSocketActivity extends Activity with Contexts[Activity]{
         view.setAdapter(arrayAdapter)
         view
     }
-
+    var wsClient: Option[WebSocket] = None
+    type WSMessage = String
     override def onCreate(savedInstanceState: Bundle): Unit ={
         super.onCreate(savedInstanceState)
         setContentView(listView)
+    }
+
+    override def onResume()={
+        super.onResume()
+        Future(connect()).start
+    }
+
+    override def onPause()={
+        super.onPause()
+        wsClient.get.sendClose()
+    }
+
+    override def onStop(): Unit ={
+        super.onStop()
+    }
+
+    def connect(): Unit = {
+        //Todo:アドレスを引数から
+        arrayAdapter.add("connecting to server")
+        arrayAdapter.notifyDataSetChanged()
+        val uri = new URI("ws://192.168.1.22:3000/session/ws/1")
+        Log.d("hoge", "running websockets...")
+        wsClient = Option(wsClient.getOrElse {
+            new WebSocketFactory().createSocket(uri).addListener(new WebSocketListener() {
+                override def onPongFrame(websocket: WebSocket, frame: WebSocketFrame): Unit = ???
+
+                override def onFrameSent(websocket: WebSocket, frame: WebSocketFrame): Unit = ???
+
+                override def onCloseFrame(websocket: WebSocket, frame: WebSocketFrame): Unit = ???
+
+                override def handleCallbackError(websocket: WebSocket, cause: Throwable): Unit = ???
+
+                override def onError(websocket: WebSocket, cause: WebSocketException): Unit = ???
+
+                override def onUnexpectedError(websocket: WebSocket, cause: WebSocketException): Unit = ???
+
+                override def onFrame(websocket: WebSocket, frame: WebSocketFrame): Unit = ???
+
+                override def onConnectError(websocket: WebSocket, cause: WebSocketException): Unit = ???
+
+                override def onBinaryFrame(websocket: WebSocket, frame: WebSocketFrame): Unit = ???
+
+                override def onTextFrame(websocket: WebSocket, frame: WebSocketFrame): Unit = {
+                    arrayAdapter.add(frame.getPayloadText)
+                    arrayAdapter.notifyDataSetChanged()
+                    listView.invalidateViews()
+                }
+
+                override def onPingFrame(websocket: WebSocket, frame: WebSocketFrame): Unit = ???
+
+                override def onTextMessageError(websocket: WebSocket, cause: WebSocketException, data: Array[Byte]): Unit = ???
+
+                override def onFrameError(websocket: WebSocket, cause: WebSocketException, frame: WebSocketFrame): Unit = ???
+
+                override def onConnected(websocket: WebSocket, headers: util.Map[WSMessage, util.List[WSMessage]]): Unit = ???
+
+                override def onSendError(websocket: WebSocket, cause: WebSocketException, frame: WebSocketFrame): Unit = ???
+
+                override def onStateChanged(websocket: WebSocket, newState: WebSocketState): Unit = ???
+
+                override def onContinuationFrame(websocket: WebSocket, frame: WebSocketFrame): Unit = ???
+
+                override def onFrameUnsent(websocket: WebSocket, frame: WebSocketFrame): Unit = ???
+
+                override def onTextMessage(websocket: WebSocket, text: WSMessage): Unit = ???
+
+                override def onBinaryMessage(websocket: WebSocket, binary: Array[Byte]): Unit = ???
+
+                override def onMessageError(websocket: WebSocket, cause: WebSocketException, frames: util.List[WebSocketFrame]): Unit = ???
+
+                override def onDisconnected(websocket: WebSocket, serverCloseFrame: WebSocketFrame, clientCloseFrame: WebSocketFrame, closedByServer: Boolean): Unit = ???
+
+            })
+        }.connect())
     }
 }
