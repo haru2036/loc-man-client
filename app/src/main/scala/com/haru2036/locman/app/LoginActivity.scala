b32298968697ef4389e85bb213af11668164167e
haru2036 <haru2036@gmail.com>
2016-01-12 23:37:49 +0900

    fixed #174

--- app/src/main/scala/com/haru2036/locman/app/LoginActivity.scala
+++ app/src/main/scala/com/haru2036/locman/app/LoginActivity.scala
@@ -3,11 +3,10 @@ package com.haru2036.locman.app
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import dispatch._
-import Defaults._
 import macroid.{ActivityContext, Contexts, IdGeneration}
 import org.persona.login.android.PersonaLoginActivity
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
   * Created by 2036 on 2016/01/11.
@@ -20,7 +19,7 @@ class LoginActivity extends Activity with Contexts[Activity] with IdGeneration{
 
     def startPersonaActivity(implicit ctx: ActivityContext) = {
         val i = new Intent(ctx.get, classOf[PersonaLoginActivity])
-        startActivityForResult(i,0)
+        startActivityForResult(i,1)
     }
 
     def startMapActivity(implicit ctx: ActivityContext) = {
@@ -29,12 +28,20 @@ class LoginActivity extends Activity with Contexts[Activity] with IdGeneration{
     }
 
     override def onActivityResult(reqCode: Int, resultCode: Int, i: Intent) = {
+        if(resultCode == Activity.RESULT_OK || reqCode == 1) {
+            Future{
+                import com.ning.http.client.AsyncHttpClientConfig
+                import com.ning.http.client.providers.netty.NettyAsyncHttpProvider
+                new NettyAsyncHttpProvider(new AsyncHttpClientConfig.Builder().build)
 
-        val assertion = i.getExtras.getString("assertion")
-        val req = url(Config.withHttp(Config.browserIDCallbackUrl ++ assertion))
-        val res = Http(req)
-        Config.appSession = Option(res.apply.getCookies.get(0).getValue)
-        startMapActivity
+                val assertion = i.getExtras.getString("assertion")
+                val req = url(Config.withHttp(Config.browserIDCallbackUrl ++ assertion))
+                val res = Http(req)
+                Config.appSession = Option(res.apply.getCookies.get(0).getValue)
+                startMapActivity
+            }.apply()
+        }
+        finish()
     }
 
 
