950a0dfb679a943a613dd33b98c7d4abd6203188
haru2036 <haru2036@gmail.com>
2016-02-05 22:13:30 +0900

    fix server address

--- app/src/main/scala/com/haru2036/locman/app/Config.scala
+++ app/src/main/scala/com/haru2036/locman/app/Config.scala
@@ -4,7 +4,7 @@ package com.haru2036.locman.app
   * Created by 2036 on 2016/01/11.
   */
 object Config{
-    val serverBaseURL = "192.168.1.22:3000"
+    val serverBaseURL = "locman.haru2036.com"
     val loginUrl = serverBaseURL ++ "/auth/login"
     val browserIDCallbackUrl = serverBaseURL ++ "/auth/page/browserid/"
     var appSession: Option[String] = None
